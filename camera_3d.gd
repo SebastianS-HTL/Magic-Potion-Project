e48b33411b70fc03f5b9dc29532d4da935787621
Sebastian Schneiderbauer <146381631+SebastianS-HTL@users.noreply.github.com>
2024-10-16 20:55:32 +0200

    add smooth dash and fix groundpound bug
    screenshape scripts messed with the slide animation

--- camera_3d.gd
+++ camera_3d.gd
@@ -3,10 +3,24 @@ extends Camera3D
 var shaking = false
 var timer = 0 
 var intens = 0
+var extrafov = 0
 
 func _process(delta: float) -> void:
-	set_fov(get_parent().get_parent().get_parent().get_child(2).get_child(2).get_child(4).get_value())
+	set_fov(get_parent().get_parent().get_parent().get_child(2).get_child(2).get_child(4).get_value()+extrafov)
 	
+	#dash
+	if get_parent().get_parent().is_dashing:
+		if extrafov <= 5:
+			extrafov += 0.5
+		else:
+			extrafov = 5
+	else:
+		if extrafov >= 5:
+			extrafov -= 0.5
+		else:
+			extrafov = 0
+	
+	#screenshake
 	if shaking:
 		if timer > 0:
 			print(timer)
@@ -18,13 +32,13 @@ func _process(delta: float) -> void:
 			
 			set_h_offset(my_random_number1)
 			set_v_offset(my_random_number2)
+		else:
+			set_h_offset(0)
+			set_v_offset(0)
 	else:
 		shaking = false
-		set_h_offset(0)
-		set_v_offset(0)
 
 func do_screen_shake(time,intensitivity):
-	print("eeeeee-received")
 	shaking = true
 	timer = time
 	intens = intensitivity
